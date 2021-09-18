9c084969a8e3d8c52046d86db1bde21b9377449a
Said Rosas <saidmift@outlook.es>
2021-09-18 17:42:20 -0500

    Add the script to validate the form

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,71 +1,70 @@
-// Validar formulario
-const inputs = document.querySelectorAll('form .campo input');
+// Validate formulario
+const inputs = document.querySelectorAll('.field__input');
 
-// Listener a los inputs
+// Inputs listener
 inputs.forEach(input => {
-    input.addEventListener('blur', validarInput);
+    input.addEventListener('blur', validateInput);
 });
 
 inputs.forEach(input => {
-    input.addEventListener('input', validarInput);
+    input.addEventListener('input', validateInput);
 });
 
-function validarInput(e) {
-    const estado = ['valido', 'no-valido'];
+// Validate function
+function validateInput(e) {
+    const state = ['is-valid', 'is-invalid'];
 
-    let clase;
+    let classState;
     if(e.target.value.length === 0) {
-        clase = estado[1];
+        classState = state[1];
     } else {
-        clase = estado[0];
+        classState = state[0];
     }
 
-    e.target.classList.remove(...estado);
-    e.target.nextElementSibling.classList.remove(...estado);
+    e.target.classList.remove(...state);
+    e.target.nextElementSibling.classList.remove(...state);
             
-    e.target.classList.add(clase);
-    e.target.nextElementSibling.classList.add(clase);
+    e.target.classList.add(classState);
+    e.target.nextElementSibling.classList.add(classState);
 
-    // Inyectar dinamicamente el div con el error en caso de que lo haya
-    if(clase === 'no-valido') {
-        // Mensaje de error
-        if(e.target.parentElement.nextElementSibling.classList[0] !== 'alerta') {
+    // Inject the div of error dinamically if it's neccesary
+    if(classState === 'is-invalid') {
+        // Error message
+        if(e.target.parentElement.nextElementSibling.classList[0] !== 'alert') {
             const errorDiv = document.createElement('div');
-            errorDiv.appendChild(document.createTextNode('Este campo es obligatorio'));
-            errorDiv.classList.add('alerta');
+            errorDiv.appendChild(document.createTextNode('This field is mandatory'));
+            errorDiv.classList.add('alert');
 
-            // Inserta el error
+            // Insert the error
             e.target.parentElement.parentElement.insertBefore(errorDiv, e.target.parentElement.nextElementSibling);
         }
     } else {
-        // Limpiar el mensaje de error si existe
-        if(e.target.parentElement.nextElementSibling.classList[0] === 'alerta') {
+        // Clean the message if exists
+        if(e.target.parentElement.nextElementSibling.classList[0] === 'alert') {
             e.target.parentElement.nextElementSibling.remove();
         }
     }
 }
 
-// Mostrar y ocultar password
-const mostrarPasswordBtn = document.querySelector('form .campo span');
-mostrarPasswordBtn.addEventListener('click', e => {
+// Show and hide password
+const showPasswordBtn = document.querySelector('.field__input-show');
+showPasswordBtn.addEventListener('click', e => {
     const passwordInput = document.querySelector('#password');
 
-    if(e.target.classList.contains('mostrar')) {
-        // mostrar el texto
-        e.target.classList.remove('mostrar');
-        // cambiar el texto
-        e.target.textContent = 'Ocultar';
-
-        // cambiamos a password
+    if(e.target.classList.contains('show')) {
+        // Show text
+        e.target.classList.remove('show');
+        // Change text
+        e.target.textContent = 'hide';
+        // Change to "show password"
         passwordInput.type = 'text';
         
     } else {
-        // mostrar el password
-        e.target.classList.add('mostrar');
-        // cambiar el texto
-        e.target.textContent = 'Mostrar';
-
-        // cambiamos a password
+        // Show password
+        e.target.classList.add('show');
+        // Change text
+        e.target.textContent = 'show';
+        // Change to "show password"
         passwordInput.type = 'password';
     }
 })
